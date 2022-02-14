4d2d2bdeb1cb51635b9ac7c1cb94a9630a0ff7cc
lisakaunitz <49461069+lisakaunitz@users.noreply.github.com>
2022-02-14 00:31:18 -0800

    Add files via upload
    setup.sh_v1

---
+++ hw1/q3/setup.sh
@@ -0,0 +1,18 @@
+# 3.2
+sudo apt install python3-pip
+sudo apt install r-base
+sudo apt install git
+# 3.3
+pip3 install pandas
+pip3 install numpy
+pip3 install ipython
+pip3 install jupyter
+pip3 install scikit-learn
+pip3 install flask
+sudo apt-get install sqlite3
+# 3.4
+bash Anaconda3-2019.10-Linux-x86_64.sh
+# 3.5
+conda install keras
+conda install tensorflow
+conda install pytorch
